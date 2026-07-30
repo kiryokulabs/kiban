3247ddb7010b6b442d1d1707f6a9dc22e13a2787
arlee17 <55693405+arlee17@users.noreply.github.com>
2026-07-30 09:42:42 +0200

    feat(backend): add projects module

--- apps/api/src/modules/project/project.module.ts
+++ apps/api/src/modules/project/project.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ProjectManager } from '@kiban/core';
+import { DatabaseModule } from '../../database/database.module';
 import { ProjectController } from './controllers/project.controller';
+import { PROJECT_MANAGER } from './interfaces/project.constants';
+import { SqliteEnvironmentRepository } from './repositories/sqlite-environment.repository';
+import { SqliteProjectRepository } from './repositories/sqlite-project.repository';
+import { SqliteProjectUnitOfWork } from './repositories/sqlite-project-unit-of-work';
 import { ProjectService } from './services/project.service';
 
-@Module({ controllers: [ProjectController], providers: [ProjectService], exports: [ProjectService] })
+@Module({
+  imports: [DatabaseModule],
+  controllers: [ProjectController],
+  providers: [
+    ProjectService,
+    SqliteProjectRepository,
+    SqliteEnvironmentRepository,
+    SqliteProjectUnitOfWork,
+    {
+      provide: PROJECT_MANAGER,
+      useFactory: (projects: SqliteProjectRepository, environments: SqliteEnvironmentRepository, unitOfWork: SqliteProjectUnitOfWork): ProjectManager => new ProjectManager(projects, environments, unitOfWork),
+      inject: [SqliteProjectRepository, SqliteEnvironmentRepository, SqliteProjectUnitOfWork]
+    }
+  ]
+})
 export class ProjectModule {}
